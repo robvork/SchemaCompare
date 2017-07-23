b4ab4252641155f659598cd83fd817be6ec66fbc
Rob Gevorkyan <robertgevorkyan@gmail.com>
2017-07-22 21:17:54 -0700

    Refresh 915PM

--- Shell/Functions/initHelperFunctions.ps1
+++ Shell/Functions/initHelperFunctions.ps1
@@ -23,57 +23,54 @@ function Initialize-SchemaCompareObjectClass
 
     $ObjectClasses = ([xml](Get-Content $ConfigFilePath -Raw)).ObjectClasses.ObjectClass 
 
-$RowSet =  ($ObjectClasses |
-              # Add quotes, trim whitespace, match name with table column name 
-              Select-Object   @{n="object_class_name"; e={"'" + $_.name.Trim() + "'"}}, 
-                              @{n="object_class_source"; e={"'" + $_.source.Trim() + "'"}},
-                              @{n="object_class_source_alias"; e={"'" + $_.source_alias.Trim() + "'"}},
-                              @{n="view_schema_name"; e={"'" + $_.view_schema.Trim() + "'"}},
-                              @{n="view_name"; e={"'" + $_.view_name.Trim() + "'"}} |
-              # Combine properties into one string separated by a comma, then a line break
-              ForEach-Object { @($_.object_class_name
-                                 $_.object_class_source 
-                                 $_.object_class_source_alias
-                                 $_.view_schema_name
-                                 $_.view_name
-                                ) -join ",`n"
-                             } | 
-              # Enclose each row with ( and )
-              ForEach-Object {
-                               @( 
-                                    "(" 
-                                     $_
-                                    ")"
-                                ) -join "`n"
-                             }) -join ",`n" # combine all rows into one string, separating
-                                            # by a comma, then a newline
-             
-$ColumnList = @(
-                    "object_class_name"
-                    "object_class_source"
-                    "object_class_source_alias"
-                    "view_schema_name"
-                    "view_name"
-               ) -join ",`n"
-$InsertHeader = @(
-                    "INSERT INTO [config].[object_class]"
-                    "("
-                        $ColumnList
-                    ")"
-                    "VALUES"
-                 ) -join "`n"
+    $RowSet =  ($ObjectClasses |
+                # Add quotes, trim whitespace, match name with table column name 
+                Select-Object   @{n="object_class_name"; e={"'" + $_.name.Trim() + "'"}}, 
+                                @{n="object_class_source"; e={"'" + $_.source.Trim() + "'"}},
+                                @{n="object_class_source_alias"; e={"'" + $_.source_alias.Trim() + "'"}},
+                                @{n="view_schema_name"; e={"'" + $_.view_schema.Trim() + "'"}},
+                                @{n="view_name"; e={"'" + $_.view_name.Trim() + "'"}} |
+                # Combine properties into one string separated by a comma, then a line break
+                ForEach-Object { @( $_.object_class_name
+                                    $_.object_class_source 
+                                    $_.object_class_source_alias
+                                    $_.view_schema_name
+                                    $_.view_name
+                                    ) -join ",`n"
+                                } | 
+                # Enclose each row with ( and )
+                ForEach-Object {
+                                @( 
+                                        "(" 
+                                        $_
+                                        ")"
+                                    ) -join "`n"
+                                }) -join ",`n" # combine all rows into one string, separating
+                                                # by a comma, then a line break
+                
+    $ColumnList = @(
+                        "object_class_name"
+                        "object_class_source"
+                        "object_class_source_alias"
+                        "view_schema_name"
+                        "view_name"
+                ) -join ",`n"
+    $InsertHeader = @(
+                        "INSERT INTO #object_class_input"
+                        "("
+                            $ColumnList
+                        ")"
+                        "VALUES"
+                    ) -join "`n"
 
-$InsertSQL = @(
-                $InsertHeader 
-                $RowSet
-              ) -join "`n"
-echo $InsertSQL 
+    $InsertSQL = @(
+                    $InsertHeader 
+                    $RowSet
+                ) -join "`n"
                     
-                    
-
-                     
-
-    $Query = "EXECUTE [config].[p_initialize_object_class]"
+    $Query = "EXECUTE [config].[p_initialize_object_class]
+                      @as_input_table_name = '#object_class_input' 
+             "
     Invoke-Sqlcmd2 -ServerInstance $ServerInstance -Database $Database -Query $Query 
 }
 
